201404c43318113953fdad0881f428dc5a00795e
lajd <lajd@mcmaster.ca>
2019-05-11 13:26:29 -0700

    fixed typo

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ parser.add_argument('es_text_field', type=str, help='Name of elasticsearch field
 parser.add_argument('save_model_pth', type=str, help='Path to binary model file')
 parser.add_argument('num_training_epochs', type=str, help='Path to binary model file')
 parser.add_argument('--must_have_fields', type=str, required=False, nargs='*', default=[],
-                    help='Fields which must be contained in the document to be elligible for keyword extraction')
+                    help='Fields which must be contained in the document to be elligible for training')
 parser.add_argument('--must_not_have_fields', type=str, required=False, nargs='*', default=[],
-                    help='Fields which must not be contained in the document to be elligible for keyword extraction')
+                    help='Fields which must not be contained in the document to be elligible for training')
 parser.add_argument('--save_vocab_model_pth', type=str, required=False, default=None, help='Path to save vocab-model. None if do not save (default)')
 parser.add_argument('--init_from_pretrained', type=bool, required=False, help='Whether to resume training a binary model')
 parser.add_argument('--pretrained_path', type=str, required=False, help='Path to binary model file')
